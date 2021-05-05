0afcc5696ded4a3d4ae3320dde36d6408c9f65ef
Mehmet Pekdemir <mehmet.pekdemir@finartz.com>
2021-05-05 19:27:19 +0300

    Category domain added.

--- category-service/src/main/java/com/mehmetpekdemir/categoryservice/domain/Category.java
+++ category-service/src/main/java/com/mehmetpekdemir/categoryservice/domain/Category.java
@@ -1,4 +1,22 @@
 package com.mehmetpekdemir.categoryservice.domain;
 
-public class Category {
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Category {
+
+    private final Long id;
+    private final String name;
+
+    public static Category withId(Long id, String name) {
+        return new Category(id, name);
+    }
+
 }
