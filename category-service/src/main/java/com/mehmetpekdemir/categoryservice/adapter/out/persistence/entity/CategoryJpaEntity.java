73ae035e7903016000962d1045ee7c6c23effece
Mehmet Pekdemir <mehmet.pekdemir@finartz.com>
2021-05-05 19:27:44 +0300

    Category jpa entity v1 added.

--- category-service/src/main/java/com/mehmetpekdemir/categoryservice/adapter/out/persistence/entity/CategoryJpaEntity.java
+++ category-service/src/main/java/com/mehmetpekdemir/categoryservice/adapter/out/persistence/entity/CategoryJpaEntity.java
@@ -1,4 +1,27 @@
 package com.mehmetpekdemir.categoryservice.adapter.out.persistence.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "category_jpa_entity")
 public class CategoryJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 50, nullable = false, unique = true)
+    private String name;
+
 }
