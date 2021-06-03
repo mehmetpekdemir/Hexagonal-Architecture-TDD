d49fcf65878953342642b303ca1ae72c60e2d0e7
Mehmet Pekdemir <mehmet.pekdemir@finartz.com>
2021-06-03 20:50:52 +0300

    Description is not unique.

--- category-service/src/main/java/com/mehmetpekdemir/categoryservice/adapter/out/persistence/entity/CategoryJpaEntity.java
+++ category-service/src/main/java/com/mehmetpekdemir/categoryservice/adapter/out/persistence/entity/CategoryJpaEntity.java
@@ -30,7 +30,7 @@ public class CategoryJpaEntity extends BaseEntity {
     @Column(name = "name", length = 50, nullable = false, unique = true)
     private String name;
 
-    @Column(name = "description", length = 50, nullable = false, unique = true)
+    @Column(name = "description", length = 50, nullable = false)
     private String description;
 
     @Enumerated(EnumType.STRING)
