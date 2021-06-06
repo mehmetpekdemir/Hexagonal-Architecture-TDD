47b58e21543361d00c914d3950d36bbe6496ffbd
Mehmet Pekdemir <mehmet.pekdemir@finartz.com>
2021-06-06 05:30:56 +0300

    rest-exception test and i18n

--- category-service/src/main/java/com/mehmetpekdemir/categoryservice/common/rest/RestExceptionHandler.java
+++ category-service/src/main/java/com/mehmetpekdemir/categoryservice/common/rest/RestExceptionHandler.java
@@ -5,11 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
 import java.util.Locale;
@@ -33,28 +38,65 @@ public class RestExceptionHandler extends BaseController {
         return createErrorResponseFromMessageSource(categoryBusinessException.getKey(), locale);
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(WebExchangeBindException.class)
-    public Response<?> handleRequestPropertyBindingError(WebExchangeBindException webExchangeBindException) {
-        log.error(webExchangeBindException.getMessage());
-        return respond(new ErrorResponse("400", webExchangeBindException.getMessage()));
-    }
-
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response<ErrorResponse> handleRequestPropertyBindingError(WebExchangeBindException webExchangeBindException, Locale locale) {
+        log.debug("Bad request!", webExchangeBindException);
+        return createFieldErrorResponse(webExchangeBindException.getBindingResult(), locale);
+    }
+
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response<ErrorResponse> handleBindException(BindException bindException, Locale locale) {
+        log.debug("Bad request!", bindException);
+        return createFieldErrorResponse(bindException.getBindingResult(), locale);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Response<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
-        log.error(methodArgumentNotValidException.getMessage());
-        return respond(new ErrorResponse("400", methodArgumentNotValidException.getBindingResult().getFieldError().getDefaultMessage()));
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response<ErrorResponse> handleInvalidArgumentException(MethodArgumentNotValidException methodArgumentNotValidException, Locale locale) {
+        log.debug("Method argument not valid. Message: $methodArgumentNotValidException.message", methodArgumentNotValidException);
+        return createFieldErrorResponse(methodArgumentNotValidException.getBindingResult(), locale);
     }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public Response<ErrorResponse> handleMethodNotSupportedException(HttpRequestMethodNotSupportedException methodNotSupportedException, Locale locale) {
+        log.debug("HttpRequestMethodNotSupportedException occurred", methodNotSupportedException);
+        return createErrorResponseFromMessageSource("common.client.methodNotSupported", locale, methodNotSupportedException.getMethod());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public Response<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException methodArgumentTypeMismatchException, Locale locale) {
+        log.trace("MethodArgumentTypeMismatchException occurred", methodArgumentTypeMismatchException);
+        return createErrorResponseFromMessageSource("common.client.typeMismatch", locale, methodArgumentTypeMismatchException.getName());
+    }
+
     @ExceptionHandler(Exception.class)
-    public Response<?> handleException(Exception exception) {
-        log.error(exception.getMessage());
-        return respond(new ErrorResponse("500", exception.getMessage()));
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Response<ErrorResponse> handleException(Exception exception, Locale locale) {
+        log.error("An error occurred! Details: ", exception);
+        return createErrorResponseFromMessageSource("common.system.error.occurred", locale);
     }
 
-    private Response<?> createErrorResponseFromMessageSource(String key, Locale locale, String... args) {
+    private Response<ErrorResponse> createFieldErrorResponse(BindingResult bindingResult, Locale locale) {
+        List<String> requiredFieldErrorMessages = retrieveLocalizationMessage("common.client.requiredField", locale);
+        String code = requiredFieldErrorMessages.get(0);
+
+        String errorMessage = bindingResult
+                .getFieldErrors().stream()
+                .map(FieldError::getField)
+                .map(error -> retrieveLocalizationMessage("common.client.requiredField", locale, error))
+                .map(errorMessageList -> errorMessageList.get(1))
+                .collect(Collectors.joining(" && "));
+
+        log.debug("Exception occurred while request validation: {}", errorMessage);
+
+        return respond(new ErrorResponse(code, errorMessage));
+    }
+
+    private Response<ErrorResponse> createErrorResponseFromMessageSource(String key, Locale locale, String... args) {
         List<String> messageList = retrieveLocalizationMessage(key, locale, args);
         return respond(new ErrorResponse(messageList.get(0), messageList.get(1)));
     }
--- category-service/src/test/java/com/mehmetpekdemir/categoryservice/common/rest/RestExceptionHandlerTest.java
+++ category-service/src/test/java/com/mehmetpekdemir/categoryservice/common/rest/RestExceptionHandlerTest.java
@@ -1,6 +1,7 @@
 package com.mehmetpekdemir.categoryservice.common.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mehmetpekdemir.categoryservice.common.exception.CategoryBusinessException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,8 +10,12 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -38,6 +43,86 @@ class RestExceptionHandlerTest {
     }
 
     @Test
+    void it_should_catch_category_business_exception() throws Exception {
+        //given
+        final var url = "/handle-category-business-exception";
+
+        //when
+        final var mvcResult = this.mockMvc.perform(get(url))
+                .andDo(print())
+                .andReturn();
+        //then
+        final var errorResponse = responseJacksonTester.parse(mvcResult.getResponse().getContentAsString()).getObject();
+        then(errorResponse.getData()).isNull();
+        then(errorResponse.getErrors().getErrorCode()).isEqualTo("400");
+        then(errorResponse.getErrors().getErrorDescription()).isEqualTo("Category already exists.");
+    }
+
+    @Test
+    void it_should_catch_bind_exception() throws Exception {
+        //given
+        final var url = "/handle-bind-exception";
+
+        //when
+        final var mvcResult = this.mockMvc.perform(get(url))
+                .andDo(print())
+                .andReturn();
+        //then
+        final var errorResponse = responseJacksonTester.parse(mvcResult.getResponse().getContentAsString()).getObject();
+        then(errorResponse.getData()).isNull();
+        then(errorResponse.getErrors().getErrorCode()).isEqualTo("1");
+        then(errorResponse.getErrors().getErrorDescription()).isEqualTo("System error occurred.");
+    }
+
+    @Test
+    void it_should_catch_method_argument_not_valid_exception() throws Exception {
+        //given
+        final var url = "/handle-method-argument-not-valid-exception";
+
+        //when
+        final var mvcResult = this.mockMvc.perform(get(url))
+                .andDo(print())
+                .andReturn();
+        //then
+        final var errorResponse = responseJacksonTester.parse(mvcResult.getResponse().getContentAsString()).getObject();
+        then(errorResponse.getData()).isNull();
+        then(errorResponse.getErrors().getErrorCode()).isEqualTo("1");
+        then(errorResponse.getErrors().getErrorDescription()).isEqualTo("System error occurred.");
+    }
+
+    @Test
+    void it_should_catch_method_not_supported_exception() throws Exception {
+        //given
+        final var url = "/handle-method-not-supported-exception";
+
+        //when
+        final var mvcResult = this.mockMvc.perform(get(url))
+                .andDo(print())
+                .andReturn();
+        //then
+        final var errorResponse = responseJacksonTester.parse(mvcResult.getResponse().getContentAsString()).getObject();
+        then(errorResponse.getData()).isNull();
+        then(errorResponse.getErrors().getErrorCode()).isEqualTo("9");
+        then(errorResponse.getErrors().getErrorDescription()).isEqualTo("Method not supported");
+    }
+
+    @Test
+    void it_should_catch_method_argument_type_mismatch_exception() throws Exception {
+        //given
+        final var url = "/handle-method-argument-type-mismatch-exception";
+
+        //when
+        final var mvcResult = this.mockMvc.perform(get(url))
+                .andDo(print())
+                .andReturn();
+        //then
+        final var errorResponse = responseJacksonTester.parse(mvcResult.getResponse().getContentAsString()).getObject();
+        then(errorResponse.getData()).isNull();
+        then(errorResponse.getErrors().getErrorCode()).isEqualTo("8");
+        then(errorResponse.getErrors().getErrorDescription()).isEqualTo("null parameter type mismatch.");
+    }
+
+    @Test
     void it_should_catch_exception() throws Exception {
         //given
         final var url = "/exception";
@@ -47,15 +132,39 @@ class RestExceptionHandlerTest {
                 .andDo(print())
                 .andReturn();
         //then
-
         final var errorResponse = responseJacksonTester.parse(mvcResult.getResponse().getContentAsString()).getObject();
         then(errorResponse.getData()).isNull();
-        then(errorResponse.getErrors().getErrorCode()).isEqualTo("500");
-        then(errorResponse.getErrors().getErrorDescription()).isEqualTo("internal server error");
+        then(errorResponse.getErrors().getErrorCode()).isEqualTo("1");
+        then(errorResponse.getErrors().getErrorDescription()).isEqualTo("System error occurred.");
     }
 
     @RestController
     private static class DummyController {
+
+        @GetMapping("/handle-category-business-exception")
+        public void handleCategoryBusinessException() throws CategoryBusinessException {
+            throw new CategoryBusinessException("category.already.exists");
+        }
+
+        @GetMapping("/handle-bind-exception")
+        public void handleBindException() throws BindException {
+            throw new BindException(null);
+        }
+
+        @GetMapping("/handle-method-argument-not-valid-exception")
+        public void handleInvalidArgumentException() throws MethodArgumentNotValidException {
+            throw new MethodArgumentNotValidException(null, null);
+        }
+
+        @GetMapping("/handle-method-not-supported-exception")
+        public void handleMethodNotSupportedException() throws HttpRequestMethodNotSupportedException {
+            throw new HttpRequestMethodNotSupportedException(null);
+        }
+
+        @GetMapping("/handle-method-argument-type-mismatch-exception")
+        public void handleMethodArgumentTypeMismatchException() throws MethodArgumentTypeMismatchException {
+            throw new MethodArgumentTypeMismatchException(null, null, null, null, null);
+        }
 
         @GetMapping("/exception")
         public void handleException() throws Exception {
